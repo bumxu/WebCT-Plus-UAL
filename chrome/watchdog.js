eeb5ec9a0a82943d67f032a3775ac24efa1b6c0a
Juande Martos <juande@bumxu.com>
2015-06-04 12:12:20 +0200

    Updating analytics code
    It works now!

--- chrome/watchdog.js
+++ chrome/watchdog.js
@@ -7,13 +7,13 @@ if (typeof localStorage.allowapplet == "undefined") {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://ssl.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-27625873-1', 'bumxu.com');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-27625873-1', 'auto');
 ga('set', 'checkProtocolTask', function(){});
 ga('require', 'displayfeatures');
 ga('send', 'pageview', {
 	'page':  '/running.html',
-	'title': 'ExBgPgWebCTPUAL'
+	'title': 'ExBgPgWebCTPUAL:0.9.21'
 });
 
 // Al iniciar, mostrar PageAction en las pestañas adecuadas
